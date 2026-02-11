5e67d4f0dc32fa3d2ba6f619fe635b8da430a3cf
Gustavo <gustavo.moya@geniusreferrals.com>
2026-02-11 15:38:01 -0500

    adding env file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require('express')
 const morgan  = require('morgan')
+
 
 const app = express()
 app.use(express.json())
@@ -99,7 +101,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })

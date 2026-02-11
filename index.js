9d2b148981192308e404fa3e04641bd9155099cd
Gustavo <gustavo.moya@geniusreferrals.com>
2026-02-11 15:51:45 -0500

    adding default endpoint

--- index.js
+++ index.js
@@ -34,6 +34,10 @@ let persons = [
     }
 ];
 
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
 app.get('/info', (request, response) => {
     response.send(`<div><p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p></div>`)
 })
